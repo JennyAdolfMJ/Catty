d08336fa0523606ead9411054a7ccb7b54ef6a39
Jennifer Li <88904004@qq.com>
2025-08-05 20:28:10 +0800

    Create a Dialogic folder

--- main_menu.gd
+++ main_menu.gd
@@ -1,7 +1,7 @@
 extends Control
 
 func _on_btn_new_game_pressed() -> void:
-	var dialog = Dialogic.start("res://chapter1.dtl");
+	var dialog = Dialogic.start("res://Dialogic//chapter1.dtl");
 	dialog.layer = 0
 	get_parent().add_game_node(dialog)
 	pass # Replace with function body.
